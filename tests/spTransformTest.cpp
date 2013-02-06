298c40eb12b5126f8de2c3d1cafb5c956477bb82
emmett9001 <emmett.butler321@gmail.com>
2013-02-06 09:32:52 -0500

    a crazier sprite test

--- tests/spTransformTest.cpp
+++ tests/spTransformTest.cpp
@@ -7,29 +7,49 @@ int main(){
         return -1;
     }
 
+    int numSprites = 20;
+    spSprite *sprites[20] = {0};
+
     spm::vec2 viewDim = frame->GetViewDimensions();
-    spSprite *sprite = new spSprite(viewDim.m[0]/2, viewDim.m[1]/2, "textures/BusinessHead_Dog_1.png");
+    for(int i = 0; i < numSprites; i++){
+        int choice = rand() % 4;
+        char *tex;
+        switch(choice){
+            case 0: tex = "textures/Cop_Head_Dog_1.png"; break;
+            case 1: tex = "textures/BusinessHead_Dog_1.png"; break;
+            case 2: tex = "textures/Nudie_Head_Dog_1.png"; break;
+            case 3: tex = "textures/Jogger_Head_Dog_1.png"; break;
+        }
+        sprites[i] = new spSprite(rand() % (int)viewDim.m[0], rand() % (int)viewDim.m[1]/2, tex);
+
+    }
     spm::vec2 anchorPoint = spm::vec2(viewDim.m[0]/2, viewDim.m[1]/2);
-    int time = 0;
+    int ttime = 0;
+
+    srand(time(NULL));
 
     do{
-        time++;
+        ttime++;
         glClear(GL_COLOR_BUFFER_BIT);
 
-        float scaler = 1.5f * (sin(.09 * time) + 2.0f);
-        sprite->SetScale(spm::vec2(scaler, scaler));
-        sprite->SetAngle(sprite->GetAngle() + .04);
-        sprite->SetPosition(spm::vec2(
-                    anchorPoint.m[0] + 400.0f * sin(.01f * time),
-                    anchorPoint.m[1] + 400.0f * cos(.01f * time))
-                );
-        sprite->Draw(frame);
+        for(int i = 0; i < numSprites; i++){
+            float scaler = 1.5f * (sin(.09 * ttime) + 2.0f);
+            sprites[i]->SetScale(spm::vec2(scaler, scaler));
+            sprites[i]->SetAngle(sprites[i]->GetAngle() + .04);
+            sprites[i]->SetPosition(spm::vec2(
+                        anchorPoint.m[0] + 20 * i * sin(i * .01f * ttime),
+                        anchorPoint.m[1] + 20 * i * cos(i * .01f * ttime))
+                    );
+            sprites[i]->Draw(frame);
+        }
 
         glfwSwapBuffers();
     } while( glfwGetKey( GLFW_KEY_ESC ) != GLFW_PRESS &&
         glfwGetWindowParam( GLFW_OPENED ) );
 
-    sprite->Dealloc();
-    delete sprite;
-    sprite = NULL;
+    for(int i = 0; i < numSprites; i++){
+        sprites[i]->Dealloc();
+        delete sprites[i];
+        sprites[i] = NULL;
+    }
 }
